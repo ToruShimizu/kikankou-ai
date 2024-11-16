13525baa96911be93cd72d7b25759dee9873fd5a
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-11-16 09:38:30 +0900

    chore: headerを追加

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const Header = () => {
+	return (
+		<header className="text-center">
+			<h1 className="text-xl font-bold tracking-wide text-gray-800 uppercase">期間工求人AI検索</h1>
+		</header>
+	);
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Header } from "./components/Header";
 import styles from "./page.module.css";
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
+			<Header />
 			<main className={styles.main}>
 				<Image className={styles.logo} src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
 				<ol>

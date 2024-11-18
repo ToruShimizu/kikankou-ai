e13868a55472f9b2ddccb6d3342a71c44853df97
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-11-19 00:19:31 +0900

    chore: gtmをサーバーで読み込む

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 
 import "./globals.css";
 const geistSans = localFont({
@@ -26,7 +26,20 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="ja" suppressHydrationWarning>
-			<GoogleTagManager gtmId={GTM_ID} />
+			<Script
+				id="gtm-script"
+				strategy="afterInteractive"
+				// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+				dangerouslySetInnerHTML={{
+					__html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+				}}
+			/>
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
 		</html>
 	);

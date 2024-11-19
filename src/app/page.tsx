9733d95400ae18501e86576152ed6b3d28ea7180
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-11-19 21:55:00 +0900

    fix: 不要なcssを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 				<Content />
 			</main>
 
-			<footer className="flex bg-white rounded-lg shadow m-4">
+			<footer className="flex bg-white rounded-lg shadow m-4 w-full">
 				<div className="w-full max-w-screen-xl mx-auto p-4">
 					<span className="block text-xs text-gray-500 sm:text-center">
 						© 2024 期間工求人AI検索 . All Rights Reserved.

2bec58b5749cc51f2bd4a01f280f5d352587df55
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-12-08 18:59:07 +0900

    chore: タイトルを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 			<footer className="flex bg-white rounded-lg shadow m-4 w-full">
 				<div className="w-full max-w-screen-xl mx-auto p-4">
 					<span className="block text-xs text-gray-500 sm:text-center">
-						© 2024 期間工求人AI検索 . All Rights Reserved.
+						© 2024 期間工求人AIサーチ . All Rights Reserved.
 					</span>
 				</div>
 			</footer>
